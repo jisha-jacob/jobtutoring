973bd8fa59f601b82043506d85fc3274f9313fe3
jisha-jacob <jisha-jacob@users.noreply.github.com>
2021-04-20 22:07:20 -0400

    Fix Course output, favicon and other cosmetics
    When job Description is empty the state showCourses is set to false.
    
    Favicon replaced.
    
    Some css modification for better UX and code cleanup.

--- src/containers/JobDesc/JobDesc.js
+++ src/containers/JobDesc/JobDesc.js
@@ -10,34 +10,34 @@ class JobDesc extends Component {
         jobDescValue: '',
         courses: null,
         coursesChosen: null,
-        showCourses: false,
-        hasError:false
+        linkDisabled: true,
+        showCourses:false,
+        hasError: false
     }
+    
     //get all course details from db
     componentDidMount() {
-
         axios.get('/courses.json')
             .then(response => {
                 this.setState({ courses: response.data });
-                console.log(this.state.courses);
+                //console.log(this.state.courses);
             })
-            .catch(error => {this.setState({hasError:true})});
-
+            .catch(error => { this.setState({ hasError: true }) });
     }
 
-    //Handler to match courses in the job description and route to skills page
+
+    //Handler to match courses in the job description and set state
     findCoursesHandler = (e) => {
         e.preventDefault();
-        if (this.state.hasError){
+        if (this.state.hasError) {
             alert("Network Error! Please try agin later.");
             return false;
         }
         const courses = this.state.courses;
         const course = Object.keys(courses);
         const description = this.state.jobDescValue.toLowerCase();
-
         const coursesChosen = [];
-        console.log(course);
+        //console.log(course);
         for (var c = 0; c < course.length; c++) {
             var skill = course[c].toLowerCase();
             if (description.search(skill) > -1) {
@@ -45,63 +45,48 @@ class JobDesc extends Component {
             }
         }
 
-        console.log(coursesChosen);
+        // console.log(coursesChosen);
         this.setState({ coursesChosen: coursesChosen });
         this.setState({ showCourses: true });
-
-
-        // this.props.history.replace('/courses');
-
-
-
     }
 
-    render() {
-        // for (let ingredientName in props.ingredients){
-        //     ingredients.push({name:ingredientName,amount:props.ingredients[ingredientName]})
-        // }
+    //Change event handler for job Description text area
+    jobDescHandler=(e)=>{
+        e.preventDefault();
+        let jobDesc=e.target.value;
+        this.setState({ jobDescValue: jobDesc }) ;
 
-        // let courseOutput=null;
-        // if (this.state.showCourses) {
-        //      courseOutput = this.state.coursesChosen.map(ig => {
-        //         return <span style={{
-        //             textTransform: 'capitalize',
-        //             display: 'block',
-        //             margin: '0 8px',
-        //             border: '1px solid #ccc',
-        //             padding: '15px'
-        //         }}
-        //             key={ig.name}><strong>{ig.name} : </strong> <a href={ig.link}>{ig.link}</a></span>
-        //     });
-        // }
-        let linkDisabled=true;
-        let title='<-- Please paste Job Description. ';
-        if (this.state.jobDescValue.length!==0){
-            linkDisabled=false;
-            title='';
-        }
-             
+        // Button should be enbaled on non empty job Description value
+        if (jobDesc.length !== 0) {
+            this.setState({ linkDisabled: false });  
+        } 
+        
+        // Button should be disabled and no Course output when Job Description is empty
+        if (jobDesc.length === 0) {
+            this.setState({ linkDisabled: true });  
+            this.setState({ showCourses: false });
+        } 
+    }
+
+
+    render() {
+    
         return (
             <React.Fragment>
-            <div className="Main">
+                <div className="Main">
+                   
+                    <div className="JobDesc">
+                        <textarea placeholder="Paste job description here." value={this.state.jobDescValue} onChange={this.jobDescHandler}></textarea>
+                        <a href='/'  onClick={this.findCoursesHandler} style={this.state.linkDisabled ? { pointerEvents: "none", background: "#808080" } : null}>
+                            <span>Find Courses</span></a>
+                    </div>
 
+                    <div className="CourseOutput">
+                        {/* {courseOutput} */}
+                        <Courses show={this.state.showCourses} coursesChosen={this.state.coursesChosen} />
+                    </div>
 
-            <div className="JobDesc">
-            
-            <textarea placeholder="Paste job description here." value={this.state.jobDescValue} onChange={(event) => this.setState({ jobDescValue: event.target.value })}></textarea>
-             
-             <a href='/' title={title} onClick={this.findCoursesHandler} style={linkDisabled ? {pointerEvents: "none",background:"#808080"} : null}>
-            <span>Find Courses</span></a>
-           
-            </div>
-           
-                <div className="CourseOutput">
-                {/* {courseOutput} */}
-                <Courses show={this.state.showCourses} coursesChosen={this.state.coursesChosen}/>
                 </div>
-              
-              </div>
-              
             </React.Fragment>
         );
     }
